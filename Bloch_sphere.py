620ab870d0313eba04bce9c523231eb0ee3a278f
ToxeyQ <tobi.ploeckinger@gmail.com>
2025-02-22 01:17:15 +0100

    Added some new experiments

--- Bloch_sphere.py
+++ Bloch_sphere.py
@@ -44,7 +44,7 @@ class Axes3DExample(ThreeDScene):
 
         sphere = Sphere(
             radius=1,
-            resolution=(18, 18)  # Adjust resolution for smoothness
+            resolution=(5, 5)  # Adjust resolution for smoothness
         )
         sphere.set_fill(opacity=0) 
 
@@ -60,7 +60,7 @@ class Axes3DExample(ThreeDScene):
         self.set_camera_orientation(phi=75 * DEGREES, theta=30 * DEGREES, zoom=2.5)
         self.add(Circle(radius=1).move_to([0,0,0]))
         #self.begin_ambient_camera_rotation(rate=0.15)
-        self.wait(4)  
+        self.wait(1)  
         
         
         
@@ -76,3 +76,40 @@ class Axes3DExample(ThreeDScene):
         self.play(t.animate.set_value(2*np.pi), run_time=2)
         self.wait(2)
         
+
+class Axes3DExample2(ThreeDScene):
+    def construct(self):
+        
+        axes = ThreeDAxes(
+            x_range=[-2, 2, 1],  # X-axis from -1 to 1
+            y_range=[-2, 2, 1],  # Y-axis from -1 to 1
+            z_range=[-2, 2, 1],  # Z-axis from -1 to 1
+            x_length=4,  # Total length of the X-axis (1 unit in each direction)
+            y_length=4,  # Total length of the Y-axis
+            z_length=4,  # Total length of the Z-axis
+        )
+
+        sphere = Sphere(
+            radius=1,
+            resolution=(5, 5)  # Adjust resolution for smoothness
+        )
+        sphere.set_fill(opacity=0) 
+        circ = Circle(radius=1).move_to([0,0,0])   
+        circ.set_fill(opacity=0.1)
+
+        arrow = Arrow3D(
+            start=np.array([0, 0, 0]),
+            end=np.array([1/np.sqrt(3),1/np.sqrt(3),1/np.sqrt(3)]),
+            resolution=20
+        )
+
+        xyproj = DashedLine([0, 0, 0], [1/np.sqrt(3), 1/np.sqrt(3), 0], color=WHITE,stroke_width=2)
+        zproj = DashedLine([1/np.sqrt(3), 1/np.sqrt(3), 0], [1/np.sqrt(3), 1/np.sqrt(3), 1/np.sqrt(3)], color=WHITE,stroke_width=2)
+
+
+
+        self.add(circ,sphere, arrow,xyproj,zproj)
+
+        self.set_camera_orientation(phi=75 * DEGREES, theta=30 * DEGREES, zoom=2)
+        self.add(axes)
+        self.wait(3)
